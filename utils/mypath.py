e645f4abd416ab7e2c63d8b23b61455302f8045f
YongwonJo <42432067+YongwonJo@users.noreply.github.com>
2024-01-24 13:06:37 +0900

    Create mypath.py

---
+++ utils/mypath.py
@@ -0,0 +1,10 @@
+class Path():
+    @staticmethod
+    def root_dir():
+        return '/workspace/HDD/AgeDB_dir'
+        """
+        Please replace 'AgeDB_dir' to your project directory.
+        """
+
+    def Divider():
+        return '/'
